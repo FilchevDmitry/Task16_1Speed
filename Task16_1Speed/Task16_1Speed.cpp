b3a56ffbbf9a20daf6dcad5be8440052606d997a
DmityFilchev <filchev_dmitry@mail.ru>
2022-05-30 14:45:46 +0300

    Добавьте файлы проекта.

---
+++ Task16_1Speed/Task16_1Speed.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include<string>
+#include<sstream>
+
+int main()
+{
+	std::stringstream text;
+	std::string deltaSpeed;
+	bool end = true;
+	do {
+		std::cout << "Enter the speed delta : ";
+		std::cin >> deltaSpeed;
+		double num = stod(deltaSpeed);
+		text.str("");
+		text << "The speed of the car : " << num;
+		std::cout << text.str() << std::endl;
+		
+	} 
+	while (end);
+}
+
